bff8be17cd0c6df35239fc04d49766b69dc071a3
root <root@localhost.localdomain>
2018-06-11 15:12:43 +0800

    xxx

--- Basic/对象和类/1.cpp
+++ Basic/对象和类/1.cpp
@@ -1,18 +1,35 @@
-#include <iostream.h>
-#include <string.h>
+#include <iostream>
+#include <string>
+#include <stdio.h>
 
 class stock{
 
 private:
 	int share_;
-	string holder_;
+	std::string holder_;
 
+public:
+	int getshare_(void)
+	{
+		return share_;		
+	}
+	std::string getholder_(void)
+	{
+		return holder_;
+	}
 
+stock(int share,std::string holder)
+{
+	share_=share;
+	holder_=holder;
 }
+
+};
 
 
 
 int main(int argc,char ** argv)
 {
-
+	class Stock *my_stock = new stock(100,"yk");
+	printf("%d\n",my_stock.getshare());
 }
