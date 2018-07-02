e39b2fcb24179a76a169c2c61f1032ed32796c67
root <root@localhost.localdomain>
2018-07-02 10:26:03 +0800

    x

--- OOP/使用类/mytime1.h
+++ OOP/使用类/mytime1.h
@@ -13,7 +13,7 @@ public:
 	Time(int hours_, int minutes_);
 	void AddMin(int m);
 	void AddHr(int h);
-	void Reset(int h = 0, int m = 0);	/*什么意思*/
+	void Reset(int h = 0, int m = 0);	/*靠靠*/
 	/*函数尾部的const表示:当前函数不能够修改本类的成员变量*/
 	/*下面三个函数，都不会修改本实例的成员变量，而是生成新的成员变量值，放入临时实例中返回*/
 	Time operator+(const Time & t) const;	
