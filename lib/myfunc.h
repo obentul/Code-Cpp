451e3b788274931890770268f25d92f157d806ac
root <root@localhost.localdomain>
2018-11-28 09:03:48 +0800

    x

---
+++ lib/myfunc.h
@@ -0,0 +1,14 @@
+#include <string>
+#include <stdio.h>
+#include <unistd.h>
+
+namespace myfunc{
+
+class MyFunc{
+
+public:
+    static int separatorstr(const char* src, char separator, int index, string &dest);
+
+}
+
+}
